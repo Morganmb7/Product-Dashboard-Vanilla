10ea95344065096de70b2ade13bfc1ef0f2d0c94
Morgan Bierbrunner <mbierbrunner@gmail.com>
2025-07-15 23:17:01 -0400

    first commit with index.html and start of cp_2.js

---
+++ cp_2.js
@@ -0,0 +1,12 @@
+function fetchProductsThen(){
+    fetch("https://www.course-api.com/javascript-store-products")
+    .then((response) => response.json())
+    .then((data) => {
+        data.forEach((product) => {
+            console.log(product.fields.name);
+        });
+})
+.catch((error) => {
+    console.error("Fetch error using then():", error);
+});
+}
